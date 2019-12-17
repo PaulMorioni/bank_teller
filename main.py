9baf6122c538660c22f81a3651635014a31d5bf3
PaulMorioni <morionipaul@gmail.com>
2019-12-16 19:11:21 -0600

    Added the generator class to create simulated ACH transactions and create filler customer and account information

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
+import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -53,12 +55,13 @@ class Customer(db.Model):
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    def __init__(self, pah, sah, acctn, bal, prod, date_opened, ave_balance, translist):
+    def __init__(self, ssns, acctn, bal, prod, date_opened, ave_balance, translist):
         #Join with Customer DB via SSN #TODO
         self.acctn = id
         self.balance = ave_balance
         self.prod = prod
         self. date_opened = date_opened
+        self.ssns = ssns
         #Join with Trans DB to call for specific Account number #TODO
 
     def print_pah():
@@ -89,4 +92,55 @@ class Trans(db.Model):
 
     
 
+
+
+
+class Generator():
+
+    def random_n_digits(n):
+        range_start = 10**(n-1)
+        range_end = (10**n)-1
+        return random.randint(range_start,range_end)
+
+
+    def gen_n_digit_unique(amount, number_of_digits):
+        list_of_numbers = []
+        for i in amount:
+            if len(list_of_numbers) = 0:
+                nnum = Generator.random_n_digits(number_of_digits)
+                list_of_numbers.append(nnum)
+            else:
+                nnum = Generator.random_n_digits(number_of_digits)
+                while nnum in list_of_numbers:
+                    nnum = Generator.random_n_digits(number_of_digits)
+                list_of_numbers.append(nnum)
+        
+        return list_of_numbers
+    
+    def random_amount():
+        amount = round(random.uniform(1.5,500.5),2)
+        return amount
+
+    def random_datetime(start, end):
+        delta = end - start
+        int_delta = (timedelta.days * 24 * 60 * 60) + timedelta.seconds
+        random_second = random.randrange(int_delta)
+        return start + timedelta(seconds=random_second)
+
+    
+
+    def gen_customer(amount):
+        customer_list = []
+        passing_ssn = []
+        active_ssn = gen_n_digit_unique(amount, 9)
+        for i in amount:
+            id = 0 + i
+            ssn = list.pop(active_ssn)
+            name = random.choice(name_list)
+            dob = random_datetime()
+            customer = [id, ssn, name, dob]
+            customer_list.append(customer)
+            passing_ssn.append(ssn)
+        return customer_list, passing_ssn
+            
 
