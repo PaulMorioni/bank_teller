9f26a8a350204ba211671d7dbe6615e3ef02b9d9
PaulMorioni <morionipaul@gmail.com>
2020-06-09 19:51:44 -0500

    added some client side form validation for balance alocreased UI support for balance page

--- static/balance.js
+++ static/balance.js
@@ -1,41 +1,66 @@
 $(function() {
-    let initTellerBalance = getTellerBalance(); //Teller Balance from server
+    let initTellerBalance = htmlToFloat($('#initTellerBalance').html()); //Teller Balance from server
     let initBalanceFormArray = getTellerBalanceForm(); //Balance form from server
     newBalance = calculateTellerBalance(initBalanceFormArray, initTellerBalance);
+    formattedTellerBal = formatBalance(newBalance);
+    $('#tellerBalance').html(formattedTellerBal);
 
     $('.balanceFormField').change(function () {
         let balanceFormArray =  getTellerBalanceForm() 
         let newBalance = calculateTellerBalance(balanceFormArray, initTellerBalance)
-        $('#tellerBalance').html(newBalance);
+        formattedTellerBal = formatBalance(newBalance);
+        $('#tellerBalance').html(formattedTellerBal);
         
     });
     
-    function createNewBalanceEntry (elementId, elementValue) {
-        elementValueFlt = htmlToFloat(elementValue)
+    function createTellerBalance (floatBalance, stringBalance) {
+        let newTellerBalance = {
+            floatBalance : floatBalance,
+            stringBalance : stringBalance
+        };
+        return newTellerBalance
+    };
+
+    function createNewBalanceEntry (elementId, elementValue, divider) {
+        let elementValueFlt
+        
+        if (typeof elementValue == 'number'){
+             elementValueFlt = elementValue
+            elementValue = (elementValue).toFixed(2);
+        } else if (typeof elementValue == 'string') {
+             elementValueFlt = parseFloat(elementValue)
+        };
 
         let newEntry = {
             elementId : elementId,
             elementValue : elementValue,
-            elementFloat : elementValueFlt
+            elementFloat : elementValueFlt,
+            divider : divider
         };
         return newEntry
     };
 
-
     function getTellerBalanceForm() {   //moves through each element and logs value as an array of objects
+        let dividers = [100,1000,50,1000,20,500,10,1000,5,100,2,100,1,100,1,25,.50,10,.25,10,.10,5,.05,2,.01,.50]
         balanceFormArray = []
         $('.balanceFormField').each( function (indexInArray, valueOfElement) {
             let elementId = $(this).attr('id');
             let elementValue = $(this).val();
-            if (['0', ''].includes((elementValue).trim())) {   //checks for empty form and fills with zeros
+            let elementDivider = dividers[indexInArray]
+
+            if (['0',''].includes((elementValue).trim()) || isNaN(elementValue)) {   //checks for empty form and fills with zeros
                 elementValue = '0.00'
+                $(this).val(elementValue)
+            } else {
+                elementValue = parseFloat(elementValue).toFixed(2)
+                elementValue = closestWholeNumber(elementValue, elementDivider)
                 $(this).val(elementValue)
             };
             if (!elementValue.includes('.')){
                 elementValue = elementValue + '.00'
                 $(this).val(elementValue)
             };
-            let balanceFormEntry = createNewBalanceEntry(elementId, elementValue);
+            let balanceFormEntry = createNewBalanceEntry(elementId, elementValue, elementDivider);
 
             balanceFormArray.push(balanceFormEntry);
         });//end for loop
@@ -49,10 +74,10 @@ $(function() {
             if ( index < 1) {
                 totalBalanced = elementObj.elementFloat
             } else {
-                totalBalanced = totalBalanced +elementObj.elementFloat
+                totalBalanced = totalBalanced + elementObj.elementFloat
             }
         }//end for loop
-        let newTellerBalanceFlt = initTellerBalance.floatBalance - totalBalanced
+        let newTellerBalanceFlt = totalBalanced - initTellerBalance
         return newTellerBalanceFlt
     }; //end calcTellerBalance
 
@@ -65,12 +90,10 @@ $(function() {
 
     };  //end teller balance constructor
 
-    function createTellerBalance (floatBalance, stringBalance) {
-        let newTellerBalance = {
-            floatBalance : floatBalance,
-            stringBalance : stringBalance
-        };
-        return newTellerBalance
+    function formatBalance(tellerBalanceFlt){
+        tellerBalanceFlt = (tellerBalanceFlt).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+        tellerBalanceFlt = '$' + tellerBalanceFlt
+        return tellerBalanceFlt
     };
 
     function htmlToFloat(htmlValue){
@@ -80,5 +103,28 @@ $(function() {
         htmlValue = parseFloat(htmlValue);
         return htmlValue
     }
+
+    function closestWholeNumber(elementValue, divider){
+        elementValue = parseFloat(elementValue);
+        elementValue = Math.round(elementValue/divider) * divider
+        elementValue = elementValue.toFixed(2)
+        return elementValue
+    };
+
+    $('#submit').click(function () { 
+        let newBalance = getTellerBalance()
+        if (newBalance.floatBalance > 0 ) {
+            alert("You're short")
+
+        } else if (newBalance.floatBalance < 0) {
+            alert("You're long")
+
+        } else if (newBalance.floatBalance = 0) {
+            alert('Congrats you balanced')
+        };
+        
+    });
+    
+    
 
 }); //close doc ready statement
